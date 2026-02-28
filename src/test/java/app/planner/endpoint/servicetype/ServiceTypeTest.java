60fcb28e538ac46a042aacdc925a0ac0566d419b
“Mario <mario.sutalo@gmail.com>
2026-02-28 12:07:33 +0100

    -cp

--- src/test/java/app/planner/endpoint/servicetype/ServiceTypeTest.java
+++ src/test/java/app/planner/endpoint/servicetype/ServiceTypeTest.java
@@ -1,5 +1,35 @@
 package app.planner.endpoint.servicetype;
 
-public class ServiceTypeTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+import app.planner.base.BaseIntegrationTest;
+import app.planner.config.TestSecurityConfig;
+import app.planner.jsontestdata.ServiceTestData;
+
+@Import(TestSecurityConfig.class)
+public class ServiceTypeTest extends BaseIntegrationTest {
+
+    @Autowired
+    private RestTestClient restTestClient;
+
+    @Test
+    void shouldReturnBadRequestForInvalidData() {
+        var invalidData = ServiceTestData.invalidNewServiceData;
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        restTestClient
+                .get()
+                .uri("api/service-types/all")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    
 
 }
